612451bffcb9025fad76f8ed34fcbb6ff6ee5c81
greatpsi <greatpsi@gmail.com>
2025-02-09 21:30:12 -0500

    Modify program to accept partial filename as command line argument.  Both the header file and data file are detived from the indicated command line arg.

--- server.py
+++ server.py
@@ -5,6 +5,7 @@ import datetime
 import random
 import aiofiles
 import json
+import sys
 
 from websockets.asyncio.server import broadcast, serve
 
@@ -27,14 +28,15 @@ async def show_time():
         
 async def get_time_delayed_data():
     await asyncio.sleep(5)
-    async with aiofiles.open("./data/rachel-headers-thin.csv", "r") as HEADERFILE:
+    headerFileName = sys.argv[1] + '-headers.csv'
+    async with aiofiles.open(headerFileName, "r") as HEADERFILE:
         headerString = await HEADERFILE.read()
         KEY_LIST = headerString.strip().split(",")
         #broadcast(CONNECTIONS, str(KEY_LIST))
         HEADERFILE.close
         
-        
-    async with aiofiles.open('./data/rachel-thin.csv') as DATAFILE:
+    dataFiileName = sys.argv[1] + '.csv'
+    async with aiofiles.open(dataFiileName) as DATAFILE:
         async for line in DATAFILE:
             value_list = line.strip().split(",")
             json_data = dict(zip(KEY_LIST, value_list))
